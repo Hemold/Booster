6cadce2c8729e52e1e2d0458246d9818ca878f59
Hemold <maltelauritsen2@gmail.com>
2024-10-09 13:41:51 +0200

    test

--- Maincode.py
+++ Maincode.py
@@ -64,13 +64,13 @@ def stop():
 
 # Main loop example
 try:
-    forward(50)  # Move forward at 50% speed
+    forward(75)  # Move forward at 50% speed
     time.sleep(2)
 
-    left(50)  # Turn left at 50% speed
+    left(75)  # Turn left at 50% speed
     time.sleep(2)
 
-    right(50)  # Turn right at 50% speed
+    right(75)  # Turn right at 50% speed
     time.sleep(2)
 
     # Sensor loop
