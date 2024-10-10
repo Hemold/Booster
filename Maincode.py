4ae0bcd39f81edcb550845ae4e26f55a48f200f8
Hemold <maltelauritsen2@gmail.com>
2024-10-10 12:47:36 +0200

    a

--- Maincode.py
+++ Maincode.py
@@ -34,11 +34,11 @@ pwm2.start(0)
 
 # Function to move forward
 def forward(speed):
-    #GPIO.output(DIR1, GPIO.LOW)  # Venstre hjul
-    #GPIO.output(DIR2, GPIO.LOW)  # Højre hjul
+    GPIO.output(DIR1, GPIO.LOW)  # Venstre hjul
+    GPIO.output(DIR2, GPIO.LOW)  # Højre hjul
     GPIO.output(DIR3, GPIO.HIGH)  # højre hjul
-    #GPIO.output(DIR4, GPIO.HIGH)  # venstre hjul
-    #pwm1.ChangeDutyCycle(speed)   # Set motor 1 speed (0-100)
+    GPIO.output(DIR4, GPIO.HIGH)  # venstre hjul
+    pwm1.ChangeDutyCycle(speed)   # Set motor 1 speed (0-100)
     pwm2.ChangeDutyCycle(speed)   # Set motor 2 speed (0-100)
 
 def backwards(speed):
