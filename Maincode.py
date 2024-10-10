38c7e57bd72f1fec52cc1c0b5f165b59fe03ad4e
Elykke <LordLykkegaard@gmail.com>
2024-10-10 14:50:56 +0200

    HAHAHA

--- Maincode.py
+++ Maincode.py
@@ -75,10 +75,10 @@ def adjust_speed(venstre, højre):
         forward(90)  # Slow down when both sensors are triggered
     elif venstre == 0 and højre == 1:
         right(70)    # Turn right
-        time.sleep(0.05)
+        time.sleep(0.1)
     elif venstre == 1 and højre == 0:
         left(70)     # Turn left
-        time.sleep(0.05)
+        time.sleep(0.1)
 
 # Cleanup GPIO
 def stop():
