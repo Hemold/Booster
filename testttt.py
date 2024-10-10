de466d1587419c678373c33d799f35abed9c0679
Hemold <maltelauritsen2@gmail.com>
2024-10-10 10:05:23 +0200

    dsadaaaa

--- testttt.py
+++ testttt.py
@@ -10,12 +10,16 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(linefollower1,GPIO.IN)
 GPIO.setup(linefollower2,GPIO.IN)
+
 try:
    while True:
         Venstre = GPIO.input(linefollower1)
-        print(Venstre)
+#        print(Venstre)
         Hoejre = GPIO.input(linefollower2)
-        print(Hoejre)
+#        print(Hoejre)
+        print(f"Venstre: {Venstre}, Højre: {Hoejre}")
+        print(f"GPIO pin {linefollower1} is {'high' if Venstre else 'low'}")
+        print(f"GPIO pin {linefollower2} is {'high' if Hoejre else 'low'}")
         time.sleep(0.1)
 except KeyboardInterrupt:
   pass
