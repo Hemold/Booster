8370e3765c297f7b257fd2b0ab78566ae555a859
Hemold <maltelauritsen2@gmail.com>
2024-10-10 10:01:39 +0200

    sdfasdas

--- testttt.py
+++ testttt.py
@@ -12,12 +12,13 @@ GPIO.setup(linefollower1,GPIO.IN)
 GPIO.setup(linefollower2,GPIO.IN)
 try:
    while True:
-    Venstre = int (GPIO.input(linefollower1))
-    print(Venstre)
-    Højre = int (GPIO.input(linefollower2))
-    print(Højre)
-    time.sleep(0.1)
+        Venstre = GPIO.input(linefollower1)
+        print(Venstre)
+        Hoejre = GPIO.input(linefollower2)
+        print(Hoejre)
+        time.sleep(0.1)
 except KeyboardInterrupt:
   pass
-GPIO.cleanup()
+finally:
+    GPIO.cleanup()
 
