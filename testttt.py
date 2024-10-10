be30e74b6df5104dda3791143fa20f361815f830
Hemold <maltelauritsen2@gmail.com>
2024-10-10 09:18:44 +0200

    Sensor test 4 ny fil

--- testttt.py
+++ testttt.py
@@ -1,1 +1,21 @@
-print("hrej")
+import RPi.GPIO as GPIO
+import time
+
+
+Sensor1_PIN = 37  # Vores sensor pin1
+Sensor2_PIN = 35  # Vores sensor pin2
+
+GPIO.setup(Sensor1_PIN, GPIO.IN)  # Sensor input
+GPIO.setup(Sensor2_PIN, GPIO.IN)
+
+
+try:
+   while True:
+        Venstre = int (GPIO.input(Sensor1_PIN))
+        print(Venstre)
+        Højre = int (GPIO.input(Sensor2_PIN))
+        print(Højre)
+        time.sleep(0.1)
+except KeyboardInterrupt:
+  pass
+GPIO.cleanup()
