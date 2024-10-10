808e69e73303f137ccaaa2ae2795aa098f86fd50
Hemold <maltelauritsen2@gmail.com>
2024-10-10 10:48:24 +0200

    dsadasea

--- testttt.py
+++ testttt.py
@@ -1,21 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-linefollower1 = 16
-#linefollower2 = 18
+linefollower1 = 31
+linefollower2 = 36
 
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(linefollower1,GPIO.IN)
-#GPIO.setup(linefollower2,GPIO.IN)
+GPIO.setup(linefollower2,GPIO.IN)
 try:
    while True:
     Venstre = int (GPIO.input(linefollower1))
     print(Venstre)
-    #Højre = int (GPIO.input(linefollower2))
-    #print(Højre)
+    Højre = int (GPIO.input(linefollower2))
+    print(Højre)
     time.sleep(0.1)
 except KeyboardInterrupt:
   pass
