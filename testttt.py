191288cbb8241af127688312d57d7521a8abbf55
Hemold <maltelauritsen2@gmail.com>
2024-10-10 09:46:41 +0200

    saesaeas

--- testttt.py
+++ testttt.py
@@ -1,22 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
-
-Sensor1_PIN = 24  # Vores sensor pin1
-Sensor2_PIN = 25  # Vores sensor pin2
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(Sensor1_PIN, GPIO.IN)  # Sensor input
-GPIO.setup(Sensor2_PIN, GPIO.IN)
+linefollower1 = 24
+linefollower2 = 25
 
 
-while True:
-    Venstre = int (GPIO.input(Sensor1_PIN))
-    Højre = int (GPIO.input(Sensor2_PIN))
-    print(Venstre) 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(linefollower1,GPIO.IN)
+GPIO.setup(linefollower2,GPIO.IN)
+try:
+   while True:
+    Venstre = int (GPIO.input(linefollower1))
+    print(Venstre)
+    Højre = int (GPIO.input(linefollower2))
     print(Højre)
     time.sleep(0.1)
-    Venstre = 0
-    Højre = 0
-#    GPIO.cleanup()
-
+except KeyboardInterrupt:
+  pass
+GPIO.cleanup()
 
